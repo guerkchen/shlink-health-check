af1bfd7de157486ed4ece704c1f4e0429f10377f
Matthias Wild <matthias@wildpeter.de>
2024-11-24 12:22:13 +0100

    code green now configured over env variableo

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,44 +1,44 @@
 const axios = require("axios")
 const TelegramBot = require("node-telegram-bot-api")
 const { app } = require('@azure/functions');
-const now = new Date();
 require("dotenv").config()
 
 const greenCheck = '\u2705'; // ✅
 const redCross = '\u274C';  // ❌
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
 
-var firstRun = true
+var lastCodeGreen = 0;
 
-function telegramMessage(msg){
+function telegramMessage(msg) {
     bot.sendMessage(process.env.TELEGRAM_GROUP_ID, msg)
 }
 
 async function fetchData(url) {
     console.log(`fetchData ${url}`)
-	var response;
-	try{
-		response = await axios.get(url)
-	} catch (error) {
-		console.error(`cannot fetch data from url ${url}`)
+    var response;
+    try {
+        response = await axios.get(url)
+    } catch (error) {
+        console.error(`cannot fetch data from url ${url}`)
         console.error(error)
         telegramMessage(`${redCross} cannot fetch data from ${url}`)
-		return null
-	}
-	return response.data
+        return null
+    }
+    return response.data
 }
 
-async function healthCheck(){
+async function healthCheck() {
     const server = process.env.SHLINK_URL
     const url = server + "/rest/health"
-	console.log(`checkServerStatus ${url}`)
+    console.log(`checkServerStatus ${url}`)
 
     const data = await fetchData(url)
-    if(data != null && data.status == "pass" && data.links != null && data.links.about == "https://shlink.io"){
+    if (data != null && data.status == "pass" && data.links != null && data.links.about == "https://shlink.io") {
         console.log(`shlink health check passed`)
-        //if (firstRun || (now.getDay() === 3 && now.getHours() === 15)) { // "i am still alive" once a week
-        telegramMessage(`${greenCheck} weekly update: shlink health check passed`)
-        //}
+        if (lastCodeGreen + process.env.TELEGRAM_CODE_GREEN_CYCLE < Date.now()) {
+            lastCodeGreen = Date.now()
+            telegramMessage(`${greenCheck} shlink health check passed`)
+        }
     } else {
         console.error(`error health check ${data}`)
         telegramMessag(`${redCross} error health check`)
@@ -48,11 +48,11 @@ async function healthCheck(){
 }
 
 app.timer("shlink-health-check", {
-	schedule: '0 0 * * * *',
-	handler: (myTimer, context) => {
-		context.log("beginnen health check on shlink")
-		healthCheck()
-	}
+    schedule: '0 0 * * * *',
+    handler: (myTimer, context) => {
+        context.log("beginnen health check on shlink")
+        healthCheck()
+    }
 })
 
 healthCheck()
