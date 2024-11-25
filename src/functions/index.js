d9b5173d6678a5e69cc6315d9263c7631e06dec9
Matthias Wild <matthias@wildpeter.de>
2024-11-25 22:33:26 +0100

    added test for redirect urls

--- src/functions/index.js
+++ src/functions/index.js
@@ -7,52 +7,119 @@ const greenCheck = '\u2705'; // ✅
 const redCross = '\u274C';  // ❌
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: false });
 
+const serverUrl = process.env.SHLINK_URL
+const redirectTable = JSON.parse(process.env.SHLINK_REDIRECT_TABLE)
+
 var lastCodeGreen = 0;
 
 function telegramMessage(msg) {
     bot.sendMessage(process.env.TELEGRAM_GROUP_ID, msg)
 }
 
-async function fetchData(url) {
-    console.log(`fetchData ${url}`)
-    var response;
-    try {
-        response = await axios.get(url)
-    } catch (error) {
-        console.error(`cannot fetch data from url ${url}`)
+function errorLog(msg, error = null) {
+    console.error(msg)
+    if (error)
         console.error(error)
-        telegramMessage(`${redCross} cannot fetch data from ${url}`)
-        return null
+    telegramMessage(`${redCross} ${msg}`)
+}
+
+function msgLog(msg) {
+    console.log(msg)
+    if (lastCodeGreen + process.env.TELEGRAM_CODE_GREEN_CYCLE < Date.now()) {
+        telegramMessage(`${greenCheck} ${msg}`)
     }
-    return response.data
+
+}
+
+function updateLastCodeGreen() {
+    if (lastCodeGreen + process.env.TELEGRAM_CODE_GREEN_CYCLE < Date.now()) {
+        lastCodeGreen = Date.now()
+    }
+}
+
+async function axiosGet(requestUrl) {
+    console.log(`axios.get ${requestUrl}`)
+    return await axios({
+        url: requestUrl,
+        method: `get`,
+        timeout: 20000,
+        maxRedirects: 0,
+        validateStatus: (status) =>
+            status >= 200 && status < 400,
+    })
+}
+
+async function checkRedirectTable() {
+    console.log(`checkRedirectTable`)
+    for (const entry of redirectTable) {
+        const url = serverUrl + "/" + entry.suffix
+        try {
+            const response = await axiosGet(serverUrl + "/" + entry.suffix)
+
+            if (response.status == 302 && response.headers.location == entry.redirect) {
+                msgLog(`successful checked url ${url} -> ${entry.redirect}`)
+            } else {
+                errorLog(`error checking url ${url}`)
+                errorLog(`response status = ${response.status}`)
+                errorLog(`${response.headers.location} ?= ${entry.redirect}`)
+            }
+        } catch (error) {
+            errorLog(`error fetch url ${url}`, error)
+        }
+    }
+}
+
+async function checkShlinkStatus() {
+    console.log(`checkShlinkStatus`)
+    const url = serverUrl + "/rest/health"
+    try {
+        const response = await axiosGet(url)
+        const data = response.data
+
+        if (data != null && data.status == "pass" && data.links != null && data.links.about == "https://shlink.io") {
+            msgLog(`shlink health check passed`)
+        } else {
+            errorLog(`error shlink health check`)
+        }
+    } catch (error) {
+        errorLog(`error fetch url ${url}`, error)
+    }
 }
 
 async function healthCheck() {
-    const server = process.env.SHLINK_URL
-    const url = server + "/rest/health"
-    console.log(`checkServerStatus ${url}`)
-
-    const data = await fetchData(url)
-    if (data != null && data.status == "pass" && data.links != null && data.links.about == "https://shlink.io") {
-        console.log(`shlink health check passed`)
-        if (lastCodeGreen + process.env.TELEGRAM_CODE_GREEN_CYCLE < Date.now()) {
-            lastCodeGreen = Date.now()
-            telegramMessage(`${greenCheck} shlink health check passed`)
-        }
-    } else {
-        console.error(`error health check ${data}`)
-        telegramMessag(`${redCross} error health check`)
-    }
-
-    firstRun = false
+    await checkShlinkStatus()
+    await checkRedirectTable()
+    updateLastCodeGreen()
 }
 
-app.timer("shlink-health-check", {
-    schedule: '0 0 * * * *',
-    handler: (myTimer, context) => {
-        context.log("beginnen health check on shlink")
-        healthCheck()
-    }
-})
+// app.timer("shlink-health-check", {
+//     schedule: '0 0 * * * *',
+//     handler: (myTimer, context) => {
+//         context.log("beginnen health check on shlink")
+//         healthCheck()
+//     }
+// })
 
 healthCheck()
+
+// async function test() {
+//     var response;
+//     try {
+//         response = await axios({
+//             url: `https://s.khs-lgz.de/test1`,
+//             method: `get`,
+//             timeout: 20000,
+//             maxRedirects: 0,
+//             validateStatus: (status) =>
+//                 status >= 200 && status < 400,
+//         })
+
+//         console.log(response.status)
+//         console.log(response.headers.location)
+//     } catch (error) {
+//         console.error(`cannot fetch data from url`)
+//         console.error(error)
+//     }
+// }
+
+// test()
